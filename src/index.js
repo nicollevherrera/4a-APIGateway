30f8bb45d5526855a9919247c76b0e2a8e9c8cc4
maoforero <maforeror@gmail.com>
2021-12-04 12:48:54 -0500

    fixing authentication error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+const authentication = require
--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -1,25 +1,30 @@
 const { method } = require('lodash');
 const fetch = require('node-fetch');
+const server = require('../server');
 const serverConfig = require('../server');
+const { ApolloError } = require('apollo-server');
 
-const authentication = async ({req} => {
+const authentication = async ({ req } => {
     const token = req.header.authorization || '';
     if(token == ''){
         return {usernameToken: null}
     }else{
-        let requestOptions ={
-            method: 'GET',
-            header: {
-                "Authorization": "token" + token
-            },
-            redirect: 'follow'
-        }
-        let response = await fetch{`${serverConfig.auth_api_url}/soporte/user/`, requestOptions}
-        if(response.status != 200){
-            console.log("Failed", response)
-            throw new ApolloError('SESION INACTIVA', 401)
-        }
-        return {usernameToken: response.json().username}
-    }
-    
+        try{
+            let requestOptions ={
+                method: 'GET',
+                header: {
+                    "Authorization": "token" + token
+                },
+                redirect: 'follow'
+            };
+            let response = await fetch(`${serverConfig.users_api_url}rest-auth/login/`, requestOptions);
+            if(response.status != 200){
+                console.log("Fallo", response)
+                throw new ApolloError("Inactive session - 401", 401);
+            }
+            return { usernameToken: response.json().username}
+        } catch(error){
+            console.log("Failed", error)
+            throw new ApolloError ("Unknow Error", 500);
+        };
 })
